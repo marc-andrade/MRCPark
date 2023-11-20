1eddacad00b8771da6b6c8652379d2df11555ffa
Marcos Andrade <marcos_q_andrade@hotmail.com>
2023-11-20 14:09:58 -0300

    feat: adição carga de dados no banco

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO VEICULO (cor, marca, modelo, placa) values ('Prata', 'Toyota', 'Corolla', 'ABC1234');
+INSERT INTO VEICULO (cor, marca, modelo, placa) values ('Branco', 'Honda', 'Civic', 'XYZ5678');
+INSERT INTO VEICULO (cor, marca, modelo, placa) values ('Preto', 'Ford', 'Focus', 'DEF9999');
